0e4dbb08312b9b3b3b33c1fa038c67b380660c0c
Flukeza07x <mr.fluke2544@gmail.com>
2024-11-25 02:17:46 +0700

    day3

--- adv-vue-stock-main samit/adv-vue-stock-main/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-
-// Import Tailwind CSS and Custome CSS file
-import './tailwind/app.css'
-import './tailwind/custom.css'
-
-import App from './App.vue'
-
-createApp(App).mount('#app')
